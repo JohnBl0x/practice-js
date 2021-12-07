f84e3e27a19886f7b491a35ef4fa42e999b64e26
John Blox <johnbloxam2@gmail.com>
2021-12-07 14:10:15 +0100

    Solved the problems with filter, map and forEach

--- day-1/day1.js
+++ day-1/day1.js
@@ -1,1 +1,39 @@
-console.log("hi")
+
+const students = [
+    {name: "Quincy", grade: 96},
+    {name: "Jason", grade: 84},
+    {name: "Alexis", grade: 100},
+    {name: "Sam", grade: 65},
+    {name: "Katie", grade: 90}
+]
+
+console.log("------------Q1------------");
+
+students.forEach(function (item) {
+    console.log(item.name + ": " + item.grade);
+})
+
+console.log("------------Q2------------");
+
+let q2 = students.map(function (item) {
+    if (item.name === "Jason") {
+        item.grade += 15
+    }
+    return item
+})
+
+console.log(q2);
+
+console.log("------------Q3------------");
+
+let q3 = students.filter(function (item) {
+    if (item.name === "Sam") {
+        return false
+    } else {
+        return true
+    }
+})
+
+console.log(q3);
+
+
